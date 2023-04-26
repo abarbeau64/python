745a5b6cfd4fbf167940a7be22c9224e349e0821
abarbeau64 <120743462+abarbeau64@users.noreply.github.com>
2023-04-26 09:11:14 -0500

    Commiting greeter.py

--- greeter.py
+++ greeter.py
@@ -23,3 +23,25 @@ def describe_pet(animal_type, pet_name):
 
 describe_pet('hamster','harry')
 describe_pet('dog','willie')
+
+def get_formatted_name(first_name, last_name, middle_name=''):
+    """REturn a full name"""
+    full_name = f"{first_name} {middle_name} {last_name}"
+    return full_name.title()
+
+musician = get_formatted_name('phil','alsomo')
+print(musician);
+
+def get_formatted_name(first_name, last_name, middle_name=''):
+    """REturn a full name"""
+    if middle_name:
+        full_name = f"{first_name} {middle_name} {last_name}"
+    else:
+        full_name = f"{first_name} {last_name}"
+    return full_name.title()
+
+musician = get_formatted_name('Dimebag', 'darrell', 'abbott')
+print(musician);
+
+musician = get_formatted_name('Steve', 'aoki')
+print(musician);
